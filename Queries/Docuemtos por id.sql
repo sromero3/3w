379d1359089608c89f28da4bb6dd796fd2061172
sromero3 <94651642+sromero3@users.noreply.github.com>
2025-02-04 11:42:56 -0400

    hoy 2

---
+++ Queries/Docuemtos por id.sql
@@ -0,0 +1,3 @@
+SELECT * FROM public.app_gestion_documentos
+WHERE app_gestion_documentos.id = 180
+ORDER BY id ASC 
---
+++ Queries/Pagos por id.sql
@@ -0,0 +1,3 @@
+SELECT * FROM public.app_gestion_pagos
+WHERE app_gestion_pagos.id = 40
+ORDER BY id ASC 
